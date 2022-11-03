2121b413c9fdf324e79cba7f02352ba45037806c
bzattera96 <brendazattera@hotmail.com>
2022-11-03 16:00:08 -0300

    Creación de proyecto Spring Boot, incluyendo cambios en el gitignore.

---
+++ moneywip/src/main/java/org/ada/moneywip/MoneywipApplication.java
@@ -0,0 +1,13 @@
+package org.ada.moneywip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneywipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneywipApplication.class, args);
+	}
+
+}
---
+++ moneywip/src/test/java/org/ada/moneywip/MoneywipApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ada.moneywip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneywipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
