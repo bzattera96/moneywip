475b1aa767b0ca58c43f5c47105d8759e347d383
bzattera96 <brendazattera@hotmail.com>
2022-11-19 22:05:00 -0300

    Creación de entidades: Persona, TipoIngreso y Objetivo.

--- src/main/java/org/ada/moneywip/entity/Persona.java
+++ src/main/java/org/ada/moneywip/entity/Persona.java
@@ -1,5 +1,112 @@
 package org.ada.moneywip.entity;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+
+@Table(name = "persona")
 public class Persona {
-    // primera entidad
+
+    @Id
+    private String dni;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column
+    private String genero;
+
+    @Column
+    private String celular;
+
+    @Column(nullable = false)
+    private int edad;
+
+    @Column(name = "recibe_ingresos", nullable = false)
+    private boolean recibeIngresos;
+
+    @Column(nullable = false)
+    private boolean estudia;
+
+    @OneToMany(mappedBy = "idPersona", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<AsignacionObjetivo> asignacionObjetivo;
+
+    @OneToMany(mappedBy = "idPersona", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<MovimientosAhorro> movimientosAhorro;
+
+    @OneToMany(mappedBy = "idPersona", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Ingreso> ingreso;
+
+    @OneToMany(mappedBy = "idPersona", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Egreso> egreso;
+
+    public Persona() {
+    }
+
+    public Persona(String dni, String nombre, String apellido, String genero, String celular, int edad, boolean recibeIngresos, boolean estudia, List<AsignacionObjetivo> asignacionObjetivo, List<MovimientosAhorro> movimientosAhorro, List<Ingreso> ingreso, List<Egreso> egreso) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.celular = celular;
+        this.edad = edad;
+        this.recibeIngresos = recibeIngresos;
+        this.estudia = estudia;
+        this.asignacionObjetivo = asignacionObjetivo;
+        this.movimientosAhorro = movimientosAhorro;
+        this.ingreso = ingreso;
+        this.egreso = egreso;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public boolean isRecibeIngresos() {
+        return recibeIngresos;
+    }
+
+    public boolean isEstudia() {
+        return estudia;
+    }
+
+    public List<AsignacionObjetivo> getAsignacionObjetivo() {
+        return asignacionObjetivo;
+    }
+
+    public List<MovimientosAhorro> getMovimientosAhorro() {
+        return movimientosAhorro;
+    }
+
+    public List<Ingreso> getIngreso() {
+        return ingreso;
+    }
+
+    public List<Egreso> getEgreso() {
+        return egreso;
+    }
 }
--- src/main/java/org/ada/moneywip/entity/TipoIngreso.java
+++ src/main/java/org/ada/moneywip/entity/TipoIngreso.java
@@ -1,4 +1,41 @@
 package org.ada.moneywip.entity;
 
+import javax.persistence.*;
+
+@Entity
+
+@Table(name = "tipo_ingreso")
+
 public class TipoIngreso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "tipo_ingreso", nullable = false)
+    private String tipoIngreso;
+
+    @OneToMany(mappedBy = "tipoIngreso", fetch = FetchType.LAZY, cascade = CascadeType.ALL) //private list de tipoingreso List<Ingresos> en onetomany
+    private List<Ingreso> ingreso;
+
+    public TipoIngreso() {
+    }
+
+    public TipoIngreso(Integer id, String tipoIngreso, List<Ingreso> ingreso) {
+        this.id = id;
+        this.tipoIngreso = tipoIngreso;
+        this.ingreso = ingreso;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTipoIngreso() {
+        return tipoIngreso;
+    }
+
+    public List<Ingreso> getIngreso() {
+        return ingreso;
+    }
 }
