f78e800ef4b10b1dc4ee46c45e0adaf0b60d7030
Aylen Silva <aylensilva26@gmail.com>
2022-11-21 21:37:32 -0300

    subo clases dto.

--- src/main/java/org/ada/moneywip/dto/EgresoDTO.java
+++ src/main/java/org/ada/moneywip/dto/EgresoDTO.java
@@ -1,4 +1,25 @@
 package org.ada.moneywip.dto;
 
 public class EgresoDTO {
+
+    private String id;
+    private String fecha;
+    private double monto;
+
+    public EgresoDTO(String fecha, double monto) { // agrego el id?
+        this.fecha = fecha;
+        this.monto = monto;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
 }
