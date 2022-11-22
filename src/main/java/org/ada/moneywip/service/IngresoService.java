0d867d30417c4133eb8c92934f90d7f1d7bf8f49
luciaespindola <lucia.espindola.gabriela@gmail.com>
2022-11-21 21:54:41 -0300

    Agrego clases service Asig. Obj. e Ingreso.

--- src/main/java/org/ada/moneywip/service/IngresoService.java
+++ src/main/java/org/ada/moneywip/service/IngresoService.java
@@ -1,4 +1,15 @@
 package org.ada.moneywip.service;
 
+import org.ada.moneywip.repository.IngresoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+
 public class IngresoService {
+
+    private final IngresoRepository ingresoRepository;
+
+    public IngresoService(IngresoRepository ingresoRepository) {
+        this.ingresoRepository = ingresoRepository;
+    }
 }
