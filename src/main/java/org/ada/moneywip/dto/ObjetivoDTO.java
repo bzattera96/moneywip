d95514b485f4d05a66454524dd8e7e7329013ec0
bzattera96 <brendazattera@hotmail.com>
2022-11-21 21:35:14 -0300

    Creación de clases DTO Persona, Objetivo y TipoIngreso.

--- src/main/java/org/ada/moneywip/dto/ObjetivoDTO.java
+++ src/main/java/org/ada/moneywip/dto/ObjetivoDTO.java
@@ -1,4 +1,36 @@
 package org.ada.moneywip.dto;
 
 public class ObjetivoDTO {
+    private Integer id;
+    private String objetivo;
+    private String fechaObj;
+    private double monto;
+    private boolean activo;
+
+    public ObjetivoDTO(String objetivo, String fechaObj, double monto, boolean activo) {
+        this.objetivo = objetivo;
+        this.fechaObj = fechaObj;
+        this.monto = monto;
+        this.activo = activo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getObjetivo() {
+        return objetivo;
+    }
+
+    public String getFechaObj() {
+        return fechaObj;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
 }
