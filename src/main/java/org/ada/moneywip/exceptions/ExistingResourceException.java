3586df90edaa849c5f5719fbbe47fcbc997c43cc
bzattera96 <brendazattera@hotmail.com>
2022-11-27 14:17:53 -0300

    Agregando la clase Persona Service y métodos. Agrego Exceptions.

---
+++ src/main/java/org/ada/moneywip/exceptions/ExistingResourceException.java
@@ -0,0 +1,12 @@
+package org.ada.moneywip.exceptions;
+
+public class ExistingResourceException extends RuntimeException {
+    public static final String MESSAGE = "El recurso que se está intentando crear, ya existe";
+
+    public ExistingResourceException() {
+    }
+
+    public ExistingResourceException(String message) {
+        super(message);
+    }
+}
