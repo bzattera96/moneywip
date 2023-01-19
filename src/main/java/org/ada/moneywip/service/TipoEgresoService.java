a542aa57fb6f8c67f05f12ccccdb6bed1b89e6f3
Aylen Silva <aylensilva26@gmail.com>
2023-01-19 20:30:47 -0300

    modificacion de clases service.

--- src/main/java/org/ada/moneywip/service/TipoEgresoService.java
+++ src/main/java/org/ada/moneywip/service/TipoEgresoService.java
@@ -1,11 +1,9 @@
 package org.ada.moneywip.service;
 
 import org.ada.moneywip.dto.TipoEgresoDTO;
-import org.ada.moneywip.dto.TipoIngresoDTO;
 import org.ada.moneywip.entity.TipoEgreso;
-import org.ada.moneywip.entity.TipoIngreso;
-import org.ada.moneywip.exceptions.ExistingResourceException;
-import org.ada.moneywip.exceptions.ResourceNotFoundException;
+import org.ada.moneywip.repository.exceptions.ExistingResourceException;
+import org.ada.moneywip.repository.exceptions.ResourceNotFoundException;
 import org.ada.moneywip.repository.TipoEgresoRepository;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
